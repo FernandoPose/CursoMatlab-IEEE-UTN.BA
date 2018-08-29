ca21535290f283ec1807f73220db9832f42830ea
Fernando Pose <fpose@MacBook-Air-de-Fernando.local>
2018-08-29 01:24:41 -0300

    Agregado ejercicio integrador clase 3

---
+++ EjerciciosIntegradores/Clase3/Enunciado/Enunciado.m
@@ -0,0 +1,41 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                         %
+% Ejercicio Integrador 2. Ordenamiento de un vector                       %
+%                                                                         %
+% Ejercicio Integrador#2                                                  %
+%                                                                         %
+% IEEE Sección Argentina UTN.BA                                           %
+%                                                                         %
+% Facultad Regional Buenos Aires                                          %
+% Universidad Tecnológica Nacional                                        %
+% Período Lectivo 2018                                                    %
+%                                                                         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% Enunciado %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                         %
+%   Ordenar el vector: x = [4 5 2 1 4 5 6 2] sin utilizar funciones       %
+%   funciones internas de Matlab                                          %
+%                                                                         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Inicializo el workspace
+clc; clear all; close all;          % Limpio el Workspace, el Command Window.
+
+%% Inicializo el vectore
+x = [4 5 2 1 4 5 6 2];
+disp(['El vector a ordenar es: ' num2str(x)]);
+
+%% Procesamiento
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% AYUDA: Utilice ciclo de repetición for y de selección if                %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%--------------------------------------------------------------------------
+% Acá código de ordenamiento
+%--------------------------------------------------------------------------
+
+%% Informe de resultados
+disp(['El vector  ordenado es: ' num2str(x)]);
+
